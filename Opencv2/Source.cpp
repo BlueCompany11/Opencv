f11bcb8fabe1dce0cd302b984a612df9dda192ae
BlueCompany11 <gronczewskim@gmail.com>
2017-10-10 20:31:34 +0200

    Problem z wylaczeniem video ktore posiada pasek przewijania

--- Opencv2/Source.cpp
+++ Opencv2/Source.cpp
@@ -1,28 +1,44 @@
 #include "opencv2/highgui.hpp"
+#include"opencv/cv.h"
 #include<iostream>
 using namespace cv;
 
-int main(int argc, char** argv) {
-	//zdjecie
-	IplImage* img = cvLoadImage(argv[1]);
-	cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
-	cvShowImage("Example1", img);
+int g_slider_position = 0;
+CvCapture* g_capture = NULL;
 
-	//film
-	cvNamedWindow("Example2", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture(argv[2]);
+void onTrackbarSlide(int pos) {
+	cvSetCaptureProperty(
+		g_capture,
+		CV_CAP_PROP_POS_FRAMES,
+		pos
+	);
+}
+
+int main(int argc, char** argv) {
+	cvNamedWindow("Example3", CV_WINDOW_AUTOSIZE);
+	g_capture = cvCreateFileCapture(argv[2]);
+	int frames = (int)cvGetCaptureProperty(
+		g_capture,
+		CV_CAP_PROP_FRAME_COUNT
+	);
+	if (frames != 0) {
+		cvCreateTrackbar(
+			"Position",
+			"Example3",
+			&g_slider_position,
+			frames,
+			onTrackbarSlide
+		);
+	}
 	IplImage* frame;
 	while (1) {
-		frame = cvQueryFrame(capture);
+		frame = cvQueryFrame(g_capture);
 		if (!frame) break;
-		cvShowImage("Example2", frame);
+		cvShowImage("Example3", frame);
 		char c = cvWaitKey(33);
-		if (c == 27) break;	//27==ESC ASCII
+		if (c == 27) break;
+		
 	}
-	cvWaitKey(0);
-	cvReleaseImage(&img);
-	cvDestroyWindow("Example1");
-
-	cvReleaseCapture(&capture);
-	cvDestroyWindow("Example2");
+	cvReleaseCapture(&g_capture);
+	cvDestroyWindow("Example3");
 }

76c1ee9525f6292ad8714f6044bb52c00474e5af
BlueCompany11 <gronczewskim@gmail.com>
2017-10-12 01:30:16 +0200

    Proste operacje na obrazie

--- Opencv2/Source.cpp
+++ Opencv2/Source.cpp
@@ -3,22 +3,49 @@
 #include<iostream>
 using namespace cv;
 
-void example2_4(IplImage* image) {
-	cvNamedWindow("Example4-in");
-	cvNamedWindow("Example4-out");
-	cvShowImage("Example4-in",image);
-	IplImage* out = cvCreateImage(
-		cvGetSize(image),
-		IPL_DEPTH_8U,
-		3
+IplImage* doPyrDown(
+	IplImage* in,
+	int filter = CV_GAUSSIAN_5x5
+) {
+	assert(in->width % 2 == 0 && in->height % 2 == 0);
+	IplImage* out = cvCreateImage(cvSize(in->width / 2, in->height / 2),
+		in->depth,
+		in->nChannels
 	);
-	cvSmooth(image, out, CV_GAUSSIAN, 3, 3);
-	cvShowImage("Example4-out", out);
-	cvReleaseImage(&out);
-	cvWaitKey(0);
-	cvDestroyAllWindows();
+	cvPyrDown(in, out);
+	return out;
+
 }
+
+IplImage* doCanny(
+	IplImage* in,
+	double lowThresh,
+	double highThresh,
+	double aperture
+) {
+	if (in->nChannels != 3)
+		return (0);
+	IplImage* out = cvCreateImage(
+		cvGetSize(in),
+		in->depth,	//IPL_DEPTH_8U,
+		1);
+	cvCanny(in, out, lowThresh, highThresh, aperture);
+	return(out);
+}
+
+
 int main(int argc, char** argv) {
-	IplImage* im = cvLoadImage(argv[1]);
-	example2_4(im);
+	cvNamedWindow("Example1");
+	IplImage* img = cvLoadImage(argv[1]);
+	IplImage* img1 = doPyrDown(img);
+	IplImage* img2 = doPyrDown(img1);
+	IplImage* img3 = doCanny(img2, 10, 100, 3);
+	cvShowImage("Example1", img3);
+	cvNamedWindow("Example2");
+	cvShowImage("Example2", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img1);
+	cvReleaseImage(&img2);
+	cvReleaseImage(&img3);
+
 }

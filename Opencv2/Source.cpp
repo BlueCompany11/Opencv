b01559fcf4608b0493850a26ccbb2bef19d4021f
BlueCompany11 <gronczewskim@gmail.com>
2017-10-12 19:06:15 +0200

    problem zmieniania wielkosci obrzau do rozwiazania na pozniej

--- Opencv2/Source.cpp
+++ Opencv2/Source.cpp
@@ -3,49 +3,20 @@
 #include<iostream>
 using namespace cv;
 
-IplImage* doPyrDown(
-	IplImage* in,
-	int filter = CV_GAUSSIAN_5x5
-) {
-	assert(in->width % 2 == 0 && in->height % 2 == 0);
-	IplImage* out = cvCreateImage(cvSize(in->width / 2, in->height / 2),
-		in->depth,
-		in->nChannels
-	);
-	cvPyrDown(in, out);
-	return out;
-
-}
-
-IplImage* doCanny(
-	IplImage* in,
-	double lowThresh,
-	double highThresh,
-	double aperture
-) {
-	if (in->nChannels != 3)
-		return (0);
-	IplImage* out = cvCreateImage(
-		cvGetSize(in),
-		in->depth,	//IPL_DEPTH_8U,
-		1);
-	cvCanny(in, out, lowThresh, highThresh, aperture);
-	return(out);
-}
-
-
 int main(int argc, char** argv) {
-	cvNamedWindow("Example1");
-	IplImage* img = cvLoadImage(argv[1]);
-	IplImage* img1 = doPyrDown(img);
-	IplImage* img2 = doPyrDown(img1);
-	IplImage* img3 = doCanny(img2, 10, 100, 3);
-	cvShowImage("Example1", img3);
-	cvNamedWindow("Example2");
-	cvShowImage("Example2", img);
-	cvWaitKey(0);
-	cvReleaseImage(&img1);
-	cvReleaseImage(&img2);
-	cvReleaseImage(&img3);
-
+	try {
+		CvSize size = CvSize(720, 640);
+		IplImage* img = cvLoadImage(argv[1]);
+		IplImage* img2 = img; //cvCreateImage(size, 1, 3);
+		cvResize(img, img2);
+		cvNamedWindow("e2");
+		cvShowImage("e2", img);
+		cvNamedWindow("e1");
+		cvShowImage("e1", img2);
+	}
+	catch (const std::exception & e) {
+		std::cout << "Doesn't work : " << e.what();
+		system("pause");
+	}
+		cvWaitKey(0);
 }
